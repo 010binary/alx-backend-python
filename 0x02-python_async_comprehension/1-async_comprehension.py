b3102bebbbf60eae3fd4153e33623ff4ce873d04
010binary <augustine32fg@gmail.com>
2024-07-09 22:51:19 +0000

    fixing takes 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,10 +4,11 @@ async comp collect 10 int
 """
 from typing import List
 import asyncio
+from importlib import import_module as check
 
 
 # Importing the other function
-async_generator = __import__('0-async_generator').async_generator
+async_generator = check('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
