945351bed161646d0f2e3368f2a2e316e9705778
010binary <augustine32fg@gmail.com>
2024-07-09 23:27:02 +0000

    working on it

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,15 +3,16 @@
 async comp collect 10 int
 """
 from typing import List
-import asyncio
 from importlib import import_module as check
 
 
 # Importing the other function
 async_generator = check('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """
     collects the 10 valuse
     """
-    return [number async for number in async_generator()]
+    result = [x async for x in async_generator()]
+    return result
